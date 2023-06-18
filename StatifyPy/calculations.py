294be560c3bb3948c3df61236b83a3920f75054e
Jay Vatti <sujay.vatti@gmail.com>
2023-06-17 23:44:24 -0400

    Update calculations.py

--- StatifyPy/calculations.py
+++ StatifyPy/calculations.py
@@ -24,39 +24,47 @@ class Calculations:
         return norm.cdf(x, mean, std_dev)
 
     @staticmethod
-    def binomial_pmf(self, k, n, p):
+    def binomial_pmf(k, n, p):
+        # success: k
+        # trails: n
+        # probability: p
         return binom.pmf(k, n, p)
 
     @staticmethod
-    def binomial_cdf(self, k, n, p):
+    def binomial_cdf(k, n, p):
+        # success: k
+        # trails: n
+        # probability: p
         return binom.cdf(k, n, p)
 
     @staticmethod
-    def t_pdf(self, x, df):
+    def t_pdf(x, df):
         return t.pdf(x, df)
 
     @staticmethod
-    def t_cdf(self, x, df):
-        return t.cdf(x, df)
+    def t_cdf(lower,upper, df):
+        return t.cdf(upper, df) - t.cdf(lower, df)
 
     # _d = default
 
-    def inv_normal_d(self, p, mean=0, std_dev=1):
+    @staticmethod
+    def inv_normal_d(p, mean=0, std_dev=1):
         return norm.ppf(p, mean, std_dev)
 
-    def inv_t_d(self, p, df):
+    @staticmethod
+    def inv_t_d(p, df):
         return t.ppf(p, df)
 
     @staticmethod
-    def inv_norm(self, p, mean=0, std_dev=1, test="LEFT"):
+    def inv_norm(p, mean=0, std_dev=1, test="DEFAULT"):
         try:
-            if test == "LEFT":
-                return self.inv_normal_d(p, mean, std_dev)
+            if test == "LEFT" or test == "DEFAULT":
+                return Calculations.inv_normal_d(p, mean, std_dev)
             elif test == "RIGHT":
-                return self.inv_normal_d(1 - p, mean, std_dev)
+                return Calculations.inv_normal_d(1 - p, mean, std_dev)
             elif test == "CENTER":
                 # p is alpha
-                return self.inv_normal_d(p / 2, mean, std_dev), self.inv_normal_d(1 - p / 2, mean, std_dev)
+                return Calculations.inv_normal_d(p / 2, mean, std_dev), Calculations.inv_normal_d(1 - p / 2, mean, std_dev)
             else:
                 raise TempError("test != [LEFT,RIGHT,CENTER]", f"INVALID TEST: {test} not DEFINED", "INV_NORM")
 
@@ -64,15 +72,15 @@ class Calculations:
             print('ERROR:', e.error, e.args[-1], e.temp, sep=': ')
 
     @staticmethod
-    def inv_t(self, p, df, test="LEFT"):
+    def inv_t(p, df, test="DEFAULT"):
         try:
-            if test == "LEFT":
-                return self.inv_t_d(p, df)
+            if test == "LEFT" or test == "DEFAULT":
+                return Calculations.inv_t_d(p, df)
             elif test == "RIGHT":
-                return self.inv_t_d(1 - p, df)
+                return Calculations.inv_t_d(1 - p, df)
             elif test == "CENTER":
                 # p is alpha
-                return self.inv_t_d(p / 2, df), self.inv_t_d(1 - p / 2, df)
+                return Calculations.inv_t_d(p / 2, df), Calculations.inv_t_d(1 - p / 2, df)
             else:
                 raise TempError("test != [LEFT,RIGHT,CENTER]", f"INVALID TEST: {test} not DEFINED", "INV_T")
 
@@ -80,12 +88,11 @@ class Calculations:
             print('ERROR:', e.error, e.args[-1], e.temp, sep=': ')
 
     @staticmethod
-    def anova(self, data, dependent_variable, factor_variable):
+    def anova(data, dependent_variable, factor_variable):
         formula = dependent_variable + ' ~ ' + factor_variable
         model = ols(formula, data=data).fit()
         anova_table = sm.stats.anova_lm(model)
         return anova_table
-
 
 
 
